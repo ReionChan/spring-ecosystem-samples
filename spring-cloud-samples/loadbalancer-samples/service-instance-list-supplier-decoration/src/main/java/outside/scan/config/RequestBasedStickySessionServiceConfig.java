cd22af686d3ccf95c634d6a0b882d000484c4761
Reion <reion78@gmail.com>
2024-06-12 01:12:55 +0800

    小版本升级验证粘性负载均衡

--- spring-cloud-samples/loadbalancer-samples/service-instance-list-supplier-decoration/src/main/java/outside/scan/config/RequestBasedStickySessionServiceConfig.java
+++ spring-cloud-samples/loadbalancer-samples/service-instance-list-supplier-decoration/src/main/java/outside/scan/config/RequestBasedStickySessionServiceConfig.java
@@ -19,8 +19,9 @@ public class RequestBasedStickySessionServiceConfig {
     @Bean
     public ServiceInstanceListSupplier requestBasedStickyServiceInstanceListSupplier(ConfigurableApplicationContext context) {
         log.info("=== RequestBasedStickyServiceConfig 设置 requestBasedStickyServiceInstanceListSupplier ===");
-        //TODO withCaching() makes sticky session not working. see https://github.com/spring-cloud/spring-cloud-commons/issues/1249
-        return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().withRequestBasedStickySession()
-                .withCaching().build(context);
+        // withCaching() makes sticky session not working. see https://github.com/spring-cloud/spring-cloud-commons/issues/1249
+        // 4.0.4 版本已修复，将 withCaching 与 withRequestBasedStickySession 互换位置
+        return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().withCaching()
+                .withRequestBasedStickySession().build(context);
     }
 }
